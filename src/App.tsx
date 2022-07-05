871614176dde3b6d69f00ed8fedae7237e039393
Barash777 <barash.vertihvost@gmail.com>
2022-07-05 11:33:49 +0300

    add uuid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {v1} from 'uuid';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          {v1()}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Todolist project for Heroku
